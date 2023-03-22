5d4d207b94f73785d0951bb4bc51dfd54a94b1fd
areeb-h <areeb.hw@gmail.com>
2023-03-22 13:58:35 +0500

    first commit

---
+++ src/main/java/com/example/enterpriseproject/EnterpriseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.enterpriseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseProjectApplication.class, args);
+	}
+
+}
