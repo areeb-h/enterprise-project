bddf3d929a7d6abcd32fc999f727183f639821cb
areeb-h <areeb.hw@gmail.com>
2023-03-30 12:21:20 +0500

    Adding database properties and user model

--- src/main/java/com/example/enterpriseproject/user/User.java
+++ src/main/java/com/example/enterpriseproject/user/User.java
@@ -1,2 +1,21 @@
-package com.example.enterpriseproject.user;public class User {
+package com.example.enterpriseproject.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
 }
