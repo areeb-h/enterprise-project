792a1dfe7ba968692fbc19614b4a8eb20547976f
areeb-h <areeb.hw@gmail.com>
2023-04-02 02:10:40 +0500

    login function implemented

--- src/main/java/com/example/enterpriseproject/controller/CustomerController.java
+++ src/main/java/com/example/enterpriseproject/controller/CustomerController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 //TODO:ALL THE MAPPINGS FOR WHAT THE CUSTOMER CAN ACCESS
 public class CustomerController {
 
-    @RequestMapping(value = {"/customer/dashboard"}, method = RequestMethod.GET)
-    public String adminHome(){
+    @RequestMapping(value = { "/customer/dashboard" }, method = RequestMethod.GET)
+    public String adminHome() {
         return "customer/dashboard";
     }
 }
